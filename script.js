7fd572dfb88d951536adebaf7676f416a157366a
Romsaeeeeee <jayceebautista0929@gmail.com>
2024-05-17 00:13:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document
-  .querySelector(".profile-img")
-  .addEventListener("mouseover", function () {
-    document.querySelector(".img-prof").src = "asset/Untitled-1.jpg";
-  });
-document
-  .querySelector(".profile-img")
-  .addEventListener("mouseout", function () {
-    document.querySelector(".img-prof").src =
-      "asset/IMG_20200808_195723_565.jpg";
-  });
+
