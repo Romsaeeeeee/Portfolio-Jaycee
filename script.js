aec6f45e0a64ed3bc237bfdcf4a8a5f1561d54b0
Romsaeeeeee <jayceebautista0929@gmail.com>
2024-05-17 01:22:32 +0800

    Final

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let web1btn = document.querySelector(".shackgrill");
+let web2btn = document.querySelector(".login");
+let cpp1 = document.querySelector(".bus");
+let cpp2 = document.querySelector(".airline");
 
+web1btn.addEventListener("click", function () {
+  window.location.href = "https://romsaeeeeee.github.io/ShackGrill/Main.html";
+});
+
+web2btn.addEventListener("click", function () {
+  window.location.href = "https://romsaeeeeee.github.io/Login-Page/login.html";
+});
+
+cpp1.addEventListener("click", function () {
+  window.location.href =
+    "https://drive.google.com/drive/u/1/folders/1LK_vps3IjWK3sGyTg7qX-Lq6LFXZMo5n";
+});
+
+cpp2.addEventListener("click", function () {
+  window.location.href =
+    "https://drive.google.com/drive/u/1/folders/1w9Mto1VYAE_yH-1UHOs0tNaC9NvKAqu4";
+});
