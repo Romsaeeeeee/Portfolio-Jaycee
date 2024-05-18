3c24bc6ce8487af71a4a880b207e0693ed2719b7
Romsaeeeeee <jayceebautista0929@gmail.com>
2024-05-18 11:44:59 +0800

    add github and blank on the link

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ let cpp1 = document.querySelector(".bus");
 let cpp2 = document.querySelector(".airline");
 
 web1btn.addEventListener("click", function () {
-  window.location.href = "https://romsaeeeeee.github.io/ShackGrill/Main.html";
+  window.open("https://romsaeeeeee.github.io/ShackGrill/Main.html", "_blank");
 });
 
 web2btn.addEventListener("click", function () {
-  window.location.href = "https://romsaeeeeee.github.io/Login-Page/login.html";
+  window.open("https://romsaeeeeee.github.io/Login-Page/login.html", "_blank");
 });
 
 cpp1.addEventListener("click", function () {
-  window.location.href =
-    "https://drive.google.com/drive/u/1/folders/1LK_vps3IjWK3sGyTg7qX-Lq6LFXZMo5n";
+  window.open(
+    "https://drive.google.com/drive/u/1/folders/1LK_vps3IjWK3sGyTg7qX-Lq6LFXZMo5n",
+    "_blank"
+  );
 });
 
 cpp2.addEventListener("click", function () {
-  window.location.href =
-    "https://drive.google.com/drive/u/1/folders/1w9Mto1VYAE_yH-1UHOs0tNaC9NvKAqu4";
+  window.open(
+    "https://drive.google.com/drive/u/1/folders/1w9Mto1VYAE_yH-1UHOs0tNaC9NvKAqu4",
+    "_blank"
+  );
 });
